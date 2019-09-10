83f050ae2aeb5011b24ae216a7d06fc88595c3d0
Samuel Arvidsson <samarv0104@skynet.halmstad.se>
2019-09-10 15:16:35 +0200

    Uppdateringn

--- Uppgifter/Uppgifter/Form1.Designer.cs
+++ Uppgifter/Uppgifter/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.btnStart = new System.Windows.Forms.Button();
             this.tbxFörhandsgranskning = new System.Windows.Forms.TextBox();
-            this.tbxTeckensnitt = new System.Windows.Forms.TextBox();
             this.cbxFet = new System.Windows.Forms.CheckBox();
             this.cbxKursiv = new System.Windows.Forms.CheckBox();
             this.cbxUnder = new System.Windows.Forms.CheckBox();
             this.tbxStorlek = new System.Windows.Forms.TextBox();
+            this.cbxStrike = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // btnStart
@@ -55,13 +56,6 @@
             this.tbxFörhandsgranskning.Size = new System.Drawing.Size(362, 169);
             this.tbxFörhandsgranskning.TabIndex = 2;
             // 
-            // tbxTeckensnitt
-            // 
-            this.tbxTeckensnitt.Location = new System.Drawing.Point(120, 235);
-            this.tbxTeckensnitt.Name = "tbxTeckensnitt";
-            this.tbxTeckensnitt.Size = new System.Drawing.Size(100, 20);
-            this.tbxTeckensnitt.TabIndex = 3;
-            // 
             // cbxFet
             // 
             this.cbxFet.AutoSize = true;
@@ -71,6 +65,7 @@
             this.cbxFet.TabIndex = 4;
             this.cbxFet.Text = "Fet";
             this.cbxFet.UseVisualStyleBackColor = true;
+            this.cbxFet.CheckedChanged += new System.EventHandler(this.CbxFet_CheckedChanged);
             // 
             // cbxKursiv
             // 
@@ -81,6 +76,7 @@
             this.cbxKursiv.TabIndex = 5;
             this.cbxKursiv.Text = "Kursiv";
             this.cbxKursiv.UseVisualStyleBackColor = true;
+            this.cbxKursiv.CheckedChanged += new System.EventHandler(this.CbxKursiv_CheckedChanged);
             // 
             // cbxUnder
             // 
@@ -91,6 +87,7 @@
             this.cbxUnder.TabIndex = 6;
             this.cbxUnder.Text = "Understruken";
             this.cbxUnder.UseVisualStyleBackColor = true;
+            this.cbxUnder.CheckedChanged += new System.EventHandler(this.CbxUnder_CheckedChanged);
             // 
             // tbxStorlek
             // 
@@ -99,20 +96,42 @@
             this.tbxStorlek.Size = new System.Drawing.Size(100, 20);
             this.tbxStorlek.TabIndex = 7;
             // 
+            // cbxStrike
+            // 
+            this.cbxStrike.AutoSize = true;
+            this.cbxStrike.Location = new System.Drawing.Point(250, 347);
+            this.cbxStrike.Name = "cbxStrike";
+            this.cbxStrike.Size = new System.Drawing.Size(53, 17);
+            this.cbxStrike.TabIndex = 8;
+            this.cbxStrike.Text = "Strike";
+            this.cbxStrike.UseVisualStyleBackColor = true;
+            this.cbxStrike.CheckedChanged += new System.EventHandler(this.CbxStrike_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(591, 66);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbxStrike);
             this.Controls.Add(this.tbxStorlek);
             this.Controls.Add(this.cbxUnder);
             this.Controls.Add(this.cbxKursiv);
             this.Controls.Add(this.cbxFet);
-            this.Controls.Add(this.tbxTeckensnitt);
             this.Controls.Add(this.tbxFörhandsgranskning);
             this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,11 +140,12 @@
         #endregion
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.TextBox tbxFörhandsgranskning;
-        private System.Windows.Forms.TextBox tbxTeckensnitt;
         private System.Windows.Forms.CheckBox cbxFet;
         private System.Windows.Forms.CheckBox cbxKursiv;
         private System.Windows.Forms.CheckBox cbxUnder;
         private System.Windows.Forms.TextBox tbxStorlek;
+        private System.Windows.Forms.CheckBox cbxStrike;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Uppgifter/Uppgifter/Form1.cs
+++ Uppgifter/Uppgifter/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Text;
 
 namespace Uppgifter
 {
@@ -19,22 +20,130 @@ namespace Uppgifter
 
         private void BtnStart_Click(object sender, EventArgs e)
         {
-            int storlek = int.Parse(tbxStorlek.Text);
-            FontStyle stil = FontStyle.Regular;
-            if (cbxFet.Checked)
+            try
             {
-                stil = stil | FontStyle.Bold;
+
+                int storlek = int.Parse(tbxStorlek.Text);
+                FontStyle stil = FontStyle.Regular;
+                if (cbxFet.Checked)
+                {
+                    stil = stil | FontStyle.Bold;
+                }
+                if (cbxKursiv.Checked)
+                {
+                    stil = stil | FontStyle.Italic;
+                }
+                if (cbxUnder.Checked)
+                {
+                    stil = stil | FontStyle.Underline;
+                }
+                if (cbxStrike.Checked)
+                {
+                    stil = stil | FontStyle.Strikeout;
+                }
+
+                string teckensnittfråncombobox = comboBox1.SelectedItem.ToString();
+
+                Font font = new Font(teckensnittfråncombobox, storlek, stil);
+                tbxFörhandsgranskning.Font = font;
             }
-            if (cbxKursiv.Checked)
+            catch(Exception ex)
             {
-                stil = stil | FontStyle.Italic;
+                MessageBox.Show("OMG!Det gick inte!. \n\n" + ex.ToString());
             }
-            if (cbxUnder.Checked)
+            InstalledFontCollection fonts = new InstalledFontCollection();
+            FontFamily[] fontas = fonts.Families.ToArray();
+            for (int i = 0; i < fontas.Length; i++)
+
             {
-                stil = stil | FontStyle.Underline;
+
+                //listBox1.Items.Add(fontas[i].Name);
+                comboBox1.Items.Add(fontas[i].Name);
+
             }
-            Font font = new Font(tbxTeckensnitt.Text, storlek, stil);
-            tbxFörhandsgranskning.Font = font;
+
+
+
+
+
+        }
+
+        private void uppdatera()
+        {
+            try
+            {
+
+                int storlek = int.Parse(tbxStorlek.Text);
+                FontStyle stil = FontStyle.Regular;
+                if (cbxFet.Checked)
+                {
+                    stil = stil | FontStyle.Bold;
+                }
+                if (cbxKursiv.Checked)
+                {
+                    stil = stil | FontStyle.Italic;
+                }
+                if (cbxUnder.Checked)
+                {
+                    stil = stil | FontStyle.Underline;
+                }
+                if (cbxStrike.Checked)
+                {
+                    stil = stil | FontStyle.Strikeout;
+                }
+
+                string teckensnittfråncombobox = comboBox1.SelectedItem.ToString();
+
+                Font font = new Font(teckensnittfråncombobox, storlek, stil);
+                tbxFörhandsgranskning.Font = font;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("OMG!Det gick inte!. \n\n" + ex.ToString());
+            }
+            InstalledFontCollection fonts = new InstalledFontCollection();
+            FontFamily[] fontas = fonts.Families.ToArray();
+            for (int i = 0; i < fontas.Length; i++)
+
+            {
+
+                //listBox1.Items.Add(fontas[i].Name);
+                comboBox1.Items.Add(fontas[i].Name);
+
+            }
+
+        }
+
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            uppdatera();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CbxUnder_CheckedChanged(object sender, EventArgs e)
+        {
+            uppdatera();
+        }
+
+        private void CbxFet_CheckedChanged(object sender, EventArgs e)
+        {
+            uppdatera();
+        }
+
+        private void CbxKursiv_CheckedChanged(object sender, EventArgs e)
+        {
+            uppdatera();
+        }
+
+        private void CbxStrike_CheckedChanged(object sender, EventArgs e)
+        {
+            uppdatera();
         }
     }
 }
