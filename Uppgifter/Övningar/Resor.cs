677d88039b1f63495d5865880d2bfd4b63992f59
Samuel Arvidsson <samarv0104@skynet.halmstad.se>
2019-09-16 09:39:01 +0200

    Har påbörjat projektet

---
+++ Uppgifter/Övningar/Resor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övningar
+{
+    class Resor
+    {
+        public string kund = "";
+        public string destination = "";
+        public int dagar = 0;
+
+
+
+    }
+}
